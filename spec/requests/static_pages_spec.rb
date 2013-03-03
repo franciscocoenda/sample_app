c9470ac5995d5e656104f2abcffc4bc3b49cfc22
Francisco Coenda <francisco.coenda@live.com>
2013-03-03 20:57:18 -0300

    Add about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -18,7 +18,7 @@ describe "Static pages" do
 
     describe "About page" do
       it "should have the content 'About Us'" do
-          visit '/static_pages/About'
+          visit '/static_pages/about'
           page.should have_content('About Us')
       end
     end
