2fb564c12b6b2984a70e27e6450c7af357b304f5
Francisco Coenda <francisco.coenda@live.com>
2013-03-02 23:29:49 -0300

    Modify static_pages_spec.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,9 +1,11 @@
 require 'spec_helper'
 
 describe "Static pages" do
-  describe "Home pages" do
+
+  describe "Home page" do
+
     it "should have the content 'Sample App'" do
-      visit '/Static_pages/home'
+      visit '/static_pages/home'
       page.should have_content('Sample App')
     end
   end
