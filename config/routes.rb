31dca10310134396a085f4857147c24b9cd57551
Francisco Coenda <francisco.coenda@live.com>
2013-04-17 21:05:04 -0300

    Modified routes.rb Delete index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   get "static_pages/home"
-  match "/help", to: "static_pages#help"
-  match "/about", to:"static_pages/about"
-  match "/contact", to: "static_pages/contact"
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 end
